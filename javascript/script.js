644f8816c80756fdc51fac4949b997754dc9e4ba
marwazyada <zyadamarwa6@gmail.com>
2022-06-10 02:11:09 +0200

    nn

--- javascript/script.js
+++ javascript/script.js
@@ -7,12 +7,16 @@ var darkMode = document.getElementById("dark-mode");
 var divMode = document.getElementById("div-mode");
 var lightMode = document.getElementById("light-mode");
 var image1 = document.getElementById("img-bg");
+let cart = document.querySelectorAll(".products-cart .fa-cart-shopping");
 
 
 
 
-displayLock.onclick = () => { menu.classList.add("show") };
-close.onclick = () => { menu.classList.remove("show") };
+displayLock.onclick = () => {
+    menu.classList.add("show");
+
+};
+
 
 darkMode.onclick = () => {
     dropMenu.style.color = "white";
@@ -39,24 +43,53 @@ lightMode.onclick = () => {
     document.body.classList.remove('darkWindow');
 }
 
+if (!(menu.classList.contains("show")) || (menu.classList.contains("show"))) {
 
 
 
+    cart.forEach((el) => el.addEventListener('click', (att) => {
+        let imgsrc = att.target.dataset.src;
+        let name = att.target.dataset.name;
+        let price = att.target.dataset.price;
 
-let cart = document.querySelectorAll(".products-cart .fa-cart-shopping");
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'pro-info d-flex justify-content-between align-items-center w-100 mt-5')
+        let newi = document.createElement('i');
+        newi.setAttribute('class', 'fa-solid fa-circle-xmark close-div')
+        newDiv.appendChild(newi)
+        let newimg = document.createElement('img')
+        newDiv.appendChild(newimg)
+        newimg.setAttribute('width', '50px')
+        newimg.setAttribute('height', '50px')
+        newimg.setAttribute('src', imgsrc)
+        let newheader1 = document.createElement('h4')
+        let newcontent1 = document.createTextNode(name)
+        newheader1.appendChild(newcontent1)
+        newDiv.appendChild(newheader1)
+        let newheader2 = document.createElement('h4')
+        let newcontent2 = document.createTextNode(price)
+        newheader2.appendChild(newcontent2)
+        newDiv.appendChild(newheader2)
 
-cart.forEach((el) => el.addEventListener('click', (att) => {
-    let src = att.target.dataset.src;
-    let name = att.target.dataset.name;
-    let price = att.target.dataset.price;
-    let cartona = `  <div id="" class="pro-info d-flex justify-content-between align-items-center w-100 mt-5">
-    <a><i  class="fa-solid fa-circle-xmark close-div"></i></a>
-  <img src="${src}"width="50px" height="50px"/>
-  <h4>${name}</h4>
-  <h4>${price}</h4>
-      </div>
-      `
-    menu.innerHTML += cartona
-    close.onclick = () => { menu.classList.remove("show") };
+        newi.onclick = () => {
+            newDiv.remove()
+        }
 
-}))
+        //         let cartona = `  <div id="" class="pro-info d-flex justify-content-between align-items-center w-100 mt-5">
+        // <i  class="fa-solid fa-circle-xmark close-div"></i>
+        // <img src="${src}"width="50px" height="50px"/>
+        // <h4>${name}</h4>
+        // <h4>${price}</h4>
+        //   </div>`
+
+        // menu.innerHTML += cartona;
+        menu.appendChild(newDiv)
+
+
+    }))
+}
+close.onclick = () => {
+
+
+    menu.classList.remove("show");
+};
